b7eb6cf6f2375439be3a21b2754ba5bbedf65c6b
Ee Kin <wongeekinn@gmail.com>
2023-07-26 14:41:50 +0800

    added rewards list bypulling data from API route

--- app/page.tsx
+++ app/page.tsx
@@ -7,26 +7,34 @@ interface Reward {
   description: string;
 }
 
-async function getData() {
-  const res = await fetch("http://localhost:3000/api/rewards");
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch data!");
-  }
-
-  return res.json();
-}
-
 export default async function Home() {
-  const data = await getData();
+  const rewards: Reward[] = await fetch(
+    "http://localhost:3000/api/rewards"
+  ).then((res) => res.json());
+
+  // return rewards;
+  // The above doesn't work! Because 'rewards' are objects, which are not valid as a React child
 
   return (
     <main className="fixed top-[150px] left-[250px] right-[50px]">
-      <div className="flex justify-around font-bold">
-        <p></p>
+      <div className="flex justify-around mb-6">
+        <div className="grid gap-4 grid-cols-3 justify-around w-full">
+          <h3 className="font-bold">Reward Name</h3>
+          <h3 className="font-bold">Category</h3>
+          <h3 className="font-bold">Description</h3>
+        </div>
       </div>
 
-      <ul></ul>
+      {rewards.map((reward) => (
+        <div
+          key={reward.id}
+          className="grid gap-4 grid-cols-3 p-4 border shadow-md"
+        >
+          <p>{reward.name}</p>
+          <p>{reward.category}</p>
+          <p>{reward.description}</p>
+        </div>
+      ))}
     </main>
   );
 }
